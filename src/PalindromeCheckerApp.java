32957d8aed056a9ebd50067ac1dcbde889132067
hemapriya10-3 <hemapriyapersn@gmail.com>
2026-02-25 20:53:48 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,9 @@
+public class PalindromeCheckerApp {
+    
+    public static void main(String[] args) {
+        System.out.println("========================================");
+        System.out.println("  WELCOME TO PALINDROME CHECKER APP");
+        System.out.println("========================================");
+        System.out.println();
+    }
+}
