18402d3885b3a2fd7cf30ff814ddd2cddfc0b1e4
hemapriya10-3 <hemapriyapersn@gmail.com>
2026-02-25 21:05:27 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,16 @@
 public class PalindromeCheckerApp {
     
     public static void main(String[] args) {
+        displayWelcome();
+    }
+    
+    // UC1: Welcome Message Module
+    public static void displayWelcome() {
         System.out.println("========================================");
         System.out.println("  WELCOME TO PALINDROME CHECKER APP");
+        System.out.println("========================================");
+        System.out.println("Check if any string is a Palindrome");
+        System.out.println("A palindrome reads the same forwards and backwards");
         System.out.println("========================================");
         System.out.println();
     }
